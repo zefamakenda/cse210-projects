3481c580e583e55651b95ec6715581fc03c1e166
zefamakenda <zefamakenda@gmail.com>
2026-01-11 09:23:47 +0200

    name and last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("Hello World! This is the Exercise1 Project. - Program.cs:7");
+
+        Console.Write("What is your first name? - Program.cs:9");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? - Program.cs:12");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. - Program.cs:15");
+
+
     }
 }

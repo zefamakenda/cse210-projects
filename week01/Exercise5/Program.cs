6cc6b171a99930dabd8d15c61ddba044216a775c
zefamakenda <zefamakenda@gmail.com>
2026-01-11 15:04:26 +0200

    exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Hello World! This is the Exercise5 Project. - Program.cs:7");
+        
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program! - Program.cs:18");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: - Program.cs:23");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: - Program.cs:29");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {squaredNumber} - Program.cs:40");
     }
 }

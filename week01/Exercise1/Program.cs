2608837197858d4489f75507c035fc6efbbd1bdc
zefamakenda <zefamakenda@gmail.com>
2026-01-11 15:14:02 +0200

    Fix console output for first name prompt

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project. ");
 
 <<<<<<< HEAD
-        Console.Write("What is your first name?  9 - Program.cs:9");
+        Console.Write("What is your first name? ");
 =======
         Console.Write("What is your first name? ");
 >>>>>>> 6d8b4a5c094b2b1a5b5e48dd9c0052f1282823c5

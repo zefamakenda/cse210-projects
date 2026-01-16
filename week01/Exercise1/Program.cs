13c89fa6d5e353d560a8d7a33909a5072326ed3f
zefamakenda <zefamakenda@gmail.com>
2026-01-16 17:37:37 +0200

    Remove extra whitespace in output messages

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,23 +1,19 @@
 using System;
 
+using System;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project. ");
+        Console.WriteLine("Hello World! This is the Exercise1 Project.");
 
-<<<<<<< HEAD
         Console.Write("What is your first name? ");
-=======
-        Console.Write("What is your first name? ");
->>>>>>> 6d8b4a5c094b2b1a5b5e48dd9c0052f1282823c5
         string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
-
-
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }

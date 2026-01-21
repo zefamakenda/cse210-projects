66052d1ef589538ffe1b135bd8d8beb04693bfe2
zefamakenda <zefamakenda@gmail.com>
2026-01-21 02:22:06 +0200

    ex 1 2 3

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,36 +1,17 @@
-<<<<<<< HEAD
+using System;
+
 class Program
 {
         static void Main(string[] args)
         {
-                Console.WriteLine("Hello World! This is the Exercise1 Project. - Program.cs:5");
+                Console.WriteLine("Hello World! This is the Exercise1 Project. - Program.cs:7");
 
-                Console.Write("What is your first name? - Program.cs:7");
+                Console.Write("What is your first name? - Program.cs:9");
                 string firstName = Console.ReadLine();
-=======
-using System;
 
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-
-        Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
->>>>>>> f9687c77b9129d0a7e4651f0e5c5cc2d80bf184c
-
-                Console.Write("What is your last name? - Program.cs:10");
+                Console.Write("What is your last name? - Program.cs:12");
                 string lastName = Console.ReadLine();
 
-<<<<<<< HEAD
-                Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. - Program.cs:13");
+                Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. - Program.cs:15");
         }
 }
-=======
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
-    }
-}
->>>>>>> f9687c77b9129d0a7e4651f0e5c5cc2d80bf184c
